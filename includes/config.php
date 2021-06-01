8ab94cf62931f2390dec2d6b91d8984b0f6dff54
RHRiday <iiucrifat@gmail.com>
2021-06-01 18:21:09 +0600

    add remote database

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,14 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','tms');
+// DB credentials. local
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','tms');
+
+define('DB_HOST','remotemysql.com');
+define('DB_USER','6inMEqJuTO');
+define('DB_PASS','YTNYHupTyF');
+define('DB_NAME','6inMEqJuTO');
 // Establish database connection.
 try
 {
