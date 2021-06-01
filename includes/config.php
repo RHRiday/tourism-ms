356170ae9e8574e233ea563272a02bff30152036
RHRiday <iiucrifat@gmail.com>
2021-06-01 18:38:45 +0600

    fix pdo connection

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,8 @@ define('DB_NAME','6inMEqJuTO');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }
 catch (PDOException $e)
 {
